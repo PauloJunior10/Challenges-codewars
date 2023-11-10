84c845d6e6e1a80b5f7e8770a54ed368f48c575c
PauloJunior <paulojunior.10@outlook.com>
2023-11-09 22:33:19 -0300

    exercicio

--- Exercises-javascript/teste.js
+++ Exercises-javascript/teste.js
@@ -1,20 +1,31 @@
-// https://www.codewars.com/kata/554b4ac871d6813a03000035/train/javascript
+// Leet Code 704 Busca binária
+// https://www.youtube.com/watch?v=ig-ZjCO8Uk8&list=PL1F2RTnUh3hZmqrlafKclRQHzNr-U0o1f&index=1&t=7s&ab_channel=CassianoVellames
 
-// Nesta pequena tarefa, você recebe uma sequência de números separados por espaços e deve retornar o número mais alto e o mais baixo.
-// Exemplos:
-// highAndLow("1 2 3 4 5");  // return "5 1"
-// highAndLow("1 2 -3 4 5"); // return "5 -3"
-// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Dado um array de inteiro chamado "nums" a qual ele é sorteado de forma ascendente, e um inteiro "target", escreva uma função para procurar o target dentro de nums. Se target existir então retorne seu index, do contrário, retorne -1.
 
-// Todos os números são válidos Int32, não há necessidade de validá-los. 
-// Sempre haverá pelo menos um número na string de entrada. 
-// A string de saída deve ser dois números separados por um único espaço, e o número mais alto é o primeiro.
+// exemplo
+// num = [1,2,3,4,5,6,7,8,9,0]
+// target = 9
+// index = 8
 
-function highAndLow(numbers){
-  numbers = numbers.split(' ');
-  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
-}
+// ------------------------------- EXERCÍCIO SIMPLES
 
 
-console.log(highAndLow("1 2 3 4 5"))
-console.log(highAndLow("27 23 3 4 35"))
+// const array = [1,2,3,4,5,6,7,8,9,10,11]
+
+// let search = (nums, target) => { // function search
+//     for (let i = 0; i< nums.length; i++){
+//         if(nums[i] === target){
+//             return i
+//         }
+//     }
+//     return -1
+// }
+
+// console.log(search(array,5))
+
+// ----------------------EXERCÍCIO COMPLEXO
+// E se eu precisar encontrar o número 435.973 em um vetor de 100000000 de posições?
+//  O método seguinte só funciona se o array estiver ordenado de forma ordenada
+
+const array = [1,2,3,4,5,6,7,8,9,10,11]
